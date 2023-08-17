5dde210d9e99b9eca89666f1909e3c0737806cc8
noyon-podder <noyon.vai369@gmail.com>
2023-08-17 17:37:17 +0600

    google sign in finish

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -6,14 +6,19 @@ import {
   signInWithEmailAndPassword,
   signOut,
   updateProfile,
+  GoogleAuthProvider,
+  signInWithPopup,
 } from "firebase/auth";
+
 import app from "../firebase/firebase.config";
 
 export const AuthContext = createContext(null);
 const auth = getAuth(app);
+
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
+  const googleProvider = new GoogleAuthProvider();
 
   const createUser = (email, password) => {
     setLoading(true);
@@ -31,10 +36,16 @@ const AuthProvider = ({ children }) => {
   };
 
   const updateUserProfile = (name, photo) => {
+    setLoading(true);
     return updateProfile(auth.currentUser, {
       displayName: name,
       photoURL: photo,
     });
+  };
+
+  const googleSignIn = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
   };
 
   useEffect(() => {
@@ -53,6 +64,7 @@ const AuthProvider = ({ children }) => {
     updateUserProfile,
     userLogin,
     userLogout,
+    googleSignIn,
   };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
---
+++ src/pages/shared/SocialLogin/SocialLogin.jsx
@@ -0,0 +1,54 @@
+import React, { useContext } from "react";
+import { FaGithub, FaGoogle } from "react-icons/fa";
+import { AuthContext } from "../../../Provider/AuthProvider";
+import { useLocation, useNavigate } from "react-router-dom";
+const SocialLogin = () => {
+  const { googleSignIn } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const from = location?.state?.from?.pathname || "/";
+
+  const handleGoogleLogin = () => {
+    googleSignIn()
+      .then((result) => {
+        const loggedInUser = result.user;
+
+        const googleUserInfo = {
+          name: loggedInUser.displayName,
+          email: loggedInUser.email,
+          photoURL: loggedInUser.photoURL,
+        };
+
+        fetch("http://localhost:5000/users", {
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify(googleUserInfo),
+        })
+          .then((res) => res.json())
+          .then(() => {
+            navigate(from, { replace: true });
+          });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  return (
+    <div className="flex items-center gap-x-8">
+      <button
+        className="btn btn-circle btn-outline"
+        onClick={handleGoogleLogin}
+      >
+        <FaGoogle className="text-xl " />
+      </button>
+      <button className="btn btn-circle btn-outline">
+        <FaGithub className="text-xl " />
+      </button>
+    </div>
+  );
+};
+
+export default SocialLogin;
